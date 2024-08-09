bcedf6ab8ec4be9aaee9299e21093862e3e3d605
Dmitriy <fstpain@gmail.com>
2024-08-10 00:02:31 +0300

    ref

--- src/features/todolistsList/ui/Todolist/Task/Task.tsx
+++ src/features/todolistsList/ui/Todolist/Task/Task.tsx
@@ -7,41 +7,41 @@ import {tasksThunks} from "../../../model/tasksSlice";
 import {useAppDispatch} from "../../../../../common/hooks";
 import {TaskType} from "../../../api/tasksApiTypes";
 
-type TaskPropsType = {
+type Props = {
     task: TaskType
     changeTaskStatus: (id: string, status: TaskStatuses, todolistId: string) => void
     changeTaskTitle: (taskId: string, newTitle: string, todolistId: string) => void
 }
 
-export const Task = React.memo((props: TaskPropsType) => {
+export const Task = React.memo(({task, changeTaskTitle, changeTaskStatus}: Props) => {
     const dispatch = useAppDispatch()
 
     const removeTaskHandler = () => dispatch(tasksThunks.removeTask({
-        taskId: props.task.id,
-        todolistId: props.task.todoListId
+        taskId: task.id,
+        todolistId: task.todoListId
     }))
 
 
     const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
         let newIsDoneValue = e.currentTarget.checked
-        props.changeTaskStatus(
-            props.task.id,
+        changeTaskStatus(
+            task.id,
             newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New,
-            props.task.todoListId,
+            task.todoListId,
         )
     }
 
     const onTitleChangeHandler = (newValue: string) => {
-        props.changeTaskTitle(props.task.id, newValue, props.task.todoListId)
+        changeTaskTitle(task.id, newValue, task.todoListId)
     }
 
 
     return (
-        <div key={props.task.id} className={props.task.status === TaskStatuses.Completed ? "is-done" : ""}>
-            <Checkbox checked={props.task.status === TaskStatuses.Completed} color="primary"
+        <div key={task.id} className={task.status === TaskStatuses.Completed ? "is-done" : ""}>
+            <Checkbox checked={task.status === TaskStatuses.Completed} color="primary"
                       onChange={onChangeHandler}/>
 
-            <EditableSpan value={props.task.title} onChange={onTitleChangeHandler}/>
+            <EditableSpan value={task.title} onChange={onTitleChangeHandler}/>
             <IconButton onClick={removeTaskHandler}>
                 <Delete/>
             </IconButton>
